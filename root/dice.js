b3842b9deea10bc81909ba27d87770f75bab5f30
SandeepGarikapati <98882254+SandeepGarikapati@users.noreply.github.com>
2024-01-11 10:27:42 +0530

    changed index and js

--- root/dice.js
+++ root/dice.js
@@ -1,13 +1,13 @@
 var random_number;
 random_number =Math.floor(Math.random()*6)+1;
 var random_src= "dice"+random_number+".png";
-var changed_src= "/Dicee Challenge - Starting Files/images/"+random_src;
+var changed_src= "/root/images/"+random_src;
 
 document.querySelectorAll("img")[0].setAttribute("src",changed_src);
 
 
 var random_number_2= Math.floor(Math.random()*6)+1;
-var changed_src = "/Dicee Challenge - Starting Files/images/"+"dice"+random_number_2+".png";
+var changed_src = "/root/images/"+"dice"+random_number_2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",changed_src);
 
 if(random_number > random_number_2)
